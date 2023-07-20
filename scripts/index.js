f2f4d0cf8ca5907ea244b53d3b52b27f702fabc8
ABDULLAHI JAMAL MOHAMED <92641182+ABDULLAHI-J@users.noreply.github.com>
2023-07-20 00:02:58 -0400

    version 1 portfolio site - first commit

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+let menuToggle = document.querySelector(".menu-toggle");
+let sidebar = document.querySelector(".sidebar");
+let navLinks = document.querySelectorAll(".menu-item");
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("is-active");
+  sidebar.classList.toggle("is-active");
+});
+
+navLinks.forEach((navItem) => {
+  navItem.addEventListener("click", () => {
+    document.querySelector(".is-active").classList.remove("is-active");
+    navItem.classList.add("is-active");
+
+    sidebar.classList.remove("is-active");
+  });
+});
